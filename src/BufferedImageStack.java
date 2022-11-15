32a065ef65440a89803b76c3a420b3d273894bfe
Sankalp <sankalp1818@gmail.com>
2022-11-15 14:39:31 -0600

    Update BufferedImageStack.java

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,4 +1,51 @@
 
+import java.awt.image.BufferedImage;
+
+
 public class BufferedImageStack {
+	
+	BufferedImage [] fin= new BufferedImage[2];
+	int size= fin.length;
+	
+	public BufferedImageStack() {
+		fin = null;
+	}
+	public BufferedImageStack(BufferedImage someBufferedImage) {
+		fin[0] = someBufferedImage;
+		
+	}
+	public void push(BufferedImage someBufferedImage) {
+		if(fin[0]==null) {
+			fin[0] = someBufferedImage;
+		}
+		else if(fin[1]==null) {
+			fin[1]= someBufferedImage;
+		}
+		else {
+
+			BufferedImage[] newer = new BufferedImage[size +1];
+			for(int i= 0; i<size+1;i++) {
+				while
+			}
+			
+		}
+		
+	}
+	public void pop() {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+	public BufferedImageStack get(int index) {
+		
+	}
+	public int getSize() {
+		
+	}
+	public int getArraySize() {
+		
+	}
 
 }
